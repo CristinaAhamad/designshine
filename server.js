2a076da65426b978e47a6a646ff9dea829b523a5
Bhavna Gupta <bhavnagupta221@gmail.com>
2019-04-30 13:23:54 -0700

    moved files into static folder

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const app = express();
 
 app.use(express.static('static_files'));
 
+const fakeDatabase = {
+	'Blacks': {swell: '5'},
+	'Shores': {swell: '3'},
+	'Scripps': {swell: '4'}
+};
+
 app.listen(3000, () => {
 	console.log('Server started at http://localhost:3000/');
 });

2de8d9a0f6034a8722f7992372ffa7445e13655a
Bhavna Gupta <bhavnagupta221@gmail.com>
2019-04-30 15:42:22 -0700

    search for specific beach

--- server.js
+++ server.js
@@ -13,6 +13,17 @@ app.get('/beaches', (req, res) => {
 	res.send(Object.keys(fakeDatabase));
 });
 
+app.get('/beaches/:name', (req, res) => {
+  const beachToLookup = req.params.name;
+  const val = fakeDatabase[beachToLookup];
+  console.log(beachToLookup, ': ', val);
+  if (val) {
+    res.send({beachToLookup, val});
+  } else {
+    res.send({beachToLookup: 'NA'});
+  }
+});
+
 app.listen(3000, () => {
 	console.log('Server started at http://localhost:3000/');
 });

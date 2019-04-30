8cfce240e49dba76d4881f96a6b47ab53a9c5604
Cristina Ahamad <cahamad@ucsd.edu>
2019-04-30 15:58:27 -0700

    firebase implementation

--- server.js
+++ server.js
@@ -3,16 +3,6 @@ const app = express();
 
 app.use(express.static('static_files'));
 
-const fakeDatabase = {
-	'Blacks': {swell: '5'},
-	'Shores': {swell: '3'},
-	'Scripps': {swell: '4'}
-};
-
-app.get('/beaches', (req, res) => {
-	res.send(Object.keys(fakeDatabase));
-});
-
 app.listen(3000, () => {
 	console.log('Server started at http://localhost:3000/');
 });

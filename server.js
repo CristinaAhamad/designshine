6604d51eae6a18b383ce9a839a3c2fec5a6d68d5
Bhavna Gupta <bhavnagupta221@gmail.com>
2019-04-30 15:57:20 -0700

    display search results

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const app = express();
 app.use(express.static('static_files'));
 
 const fakeDatabase = {
-	'Blacks': {swell: '5'},
-	'Shores': {swell: '3'},
-	'Scripps': {swell: '4'}
+	'Blacks': {swell: '5', tide: '11', wind: '4mph', temp: '60'},
+	'Shores': {swell: '3', tide: '4', wind: '7mph', temp: '58'},
+	'Scripps': {swell: '4', tide: '7', wind: '10mph', temp: '58'}
 };
 
 app.get('/beaches', (req, res) => {

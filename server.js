9c15dd1f937bac2f8d2faa55bc1adb1931e89e08
Cristina Ahamad <cahamad@ucsd.edu>
2019-05-28 01:41:04 -0700

    cors error FIXED

--- server.js
+++ server.js
@@ -3,21 +3,11 @@ const app = express();
 
 app.use(express.static('static_files'));
 
-/*const fakeDatabase = {
-	'Blacks': {swell: '5', tide: '11', wind: '4mph', temp: '60'},
-	'Shores': {swell: '3', tide: '4', wind: '7mph', temp: '58'},
-	'Scripps': {swell: '4', tide: '7', wind: '10mph', temp: '58'}
-};*/
-
-app.get('/beaches', (req, res) => {
-	res.send();
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
-
-app.get('/beaches/:name', (req, res) => {
-	console.log("made it here");
-});
-
-
 
 app.listen(3000, () => {
 	console.log('Server started at http://localhost:3000/');

b5a1cefb38c61ebeac89c0869388a52e0e6b3144
Jenna Ohira <johira@ucsd.edu>
2019-06-04 23:58:21 -0700

    script.js description

--- static_files/script.js
+++ static_files/script.js
@@ -1,29 +1,32 @@
+/*
+Connects to the spitcast API to get a list of beaches in San Diego County.
+It then displays the beach names in a list on the home page of Tidekick
+for the user to read and search through.
+*/
 const search = document.getElementById('searchOptions');
-//let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 let req = new XMLHttpRequest();
 
-//http://magicseaweed.com/api/797022276644-6ludpo55lhr67659aqrvu678sftkevis.apps.googleusercontent.com/forecast/?spot_id=10
 req.open('GET', 'http://api.spitcast.com/api/county/spots/san-diego/', true);
 req.responseType = 'text';
 
 req.onload = function () {
 	var data = JSON.parse(this.responseText);
 
+	//create list of beaches
 	const h1 = document.createElement('h1');
 	h1.textContent = "Beaches Near You:";
 	h1.setAttribute("style", "line-height: 10px; font-size: 30px");
 	search.appendChild(h1);
 
 	data.forEach(beach => {
-		console.log("spot:", beach.spot_name);
-		//check if beach name is 1 or 2+ words
+		//check if beach name is 1 or 2+ words to use for id
 		var beachName = beach.spot_name;
 		var words = beachName.split(' ').filter(function(v){return v!==''});
 		if (words.length > 1) {
 			beachName = beachName.replace(/ /g, '');
 		}
 
-		//Beach names for search
+		//Display beach names for search
 		const card = document.createElement('div');
 		card.setAttribute('class', 'card');
 		const link = document.createElement('button');
@@ -35,7 +38,6 @@ req.onload = function () {
 		link.setAttribute('class', 'linkButton');
 		link.setAttribute('id', beachName);
 		link.setAttribute("style", "line-height: 40px; font-size: 20px;");
-		//link.href = "weatherdata.html";
 
 		var collapsed = document.createElement('div');
 		collapsed.setAttribute('class', 'collapse');
